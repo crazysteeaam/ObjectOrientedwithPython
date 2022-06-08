9c8ee77203f5fc6ef5550606094a278c914a118b
crazysteeaam <jerryxuzhengjie@gmail.com>
2022-06-09 01:33:35 +0800

    0609Changes

---
+++ pythonGUI/python-bigwork/taskdetail_package/taskdetail_data.py
@@ -0,0 +1,84 @@
+import pymysql
+
+
+def get_roomcompletesitui_fromtask(taskid: str):
+    """
+    获取任务中的完成情况
+    """
+    conn = pymysql.connect(host='sh-cdb-3chov2j0.sql.tencentcdb.com', port=58932, user='root', password='Qqhh11191911',
+                           database='obj_bigwork', charset='utf8')
+    cursor = conn.cursor()
+    sql = "call get_roomcompletesitui_fromtask(%s)"
+    try:
+        cursor.execute(sql, taskid)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print("Error:" + str(e))
+        return False
+    finally:
+        conn.close()
+
+
+def get_taskname(taskid: str):
+    """
+    获取任务名
+    """
+    conn = pymysql.connect(host='sh-cdb-3chov2j0.sql.tencentcdb.com', port=58932, user='root', password='Qqhh11191911',
+                           database='obj_bigwork', charset='utf8')
+    cursor = conn.cursor()
+    sql = "select TaskContent from Task where taskid=%s"
+    try:
+        cursor.execute(sql, taskid)
+        result = cursor.fetchone()
+        return result
+    except Exception as e:
+        print("Error:" + str(e))
+        return False
+    finally:
+        conn.close()
+
+
+def check_complete(taskid, roomid) -> bool:
+    """
+    已读任务
+    """
+    conn = pymysql.connect(host='sh-cdb-3chov2j0.sql.tencentcdb.com', port=58932, user='root', password='Qqhh11191911',
+                           database='obj_bigwork', charset='utf8')
+    cursor = conn.cursor()
+    sql = "update PicURL set isConfirmed=1 where TaskID = %s and StudentCode in (select StudentCode from Students " \
+          "where RoomID = %s) "
+    try:
+        cursor.execute(sql, (taskid, roomid))
+        conn.commit()
+        return True
+    except Exception as e:
+        print("Error:" + str(e))
+        return False
+    finally:
+        conn.close()
+
+
+def delete_complete(taskid, roomid) -> bool:
+    """
+    打回任务
+    """
+    conn = pymysql.connect(host='sh-cdb-3chov2j0.sql.tencentcdb.com', port=58932, user='root', password='Qqhh11191911',
+                           database='obj_bigwork', charset='utf8')
+    cursor = conn.cursor()
+    sql = "delete from PicURL where TaskID = %s and StudentCode in (select StudentCode from Students " \
+          "where RoomID = %s) "
+    try:
+        cursor.execute(sql, (taskid, roomid))
+        conn.commit()
+        return True
+    except Exception as e:
+        print("Error:" + str(e))
+        return False
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    check_complete('2', '1')
+    print("ok")
